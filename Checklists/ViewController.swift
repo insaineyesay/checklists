31dc51efa9279a0e225bdbfcd9be3212aa5df2c9
Michael Agee <insaineyesay@gmail.com>
2018-10-29 19:01:37 -0400

    initial commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Michael Agee on 10/29/18.
+//  Copyright © 2018 Ajira LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
