3afb36fdb0b7fd44df242816a08d0f89a5670819
Michael Agee <insaineyesay@gmail.com>
2018-11-04 13:21:20 -0500

    configuring the done button

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,56 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Michael Agee on 11/4/18.
+//  Copyright © 2018 Ajira LLC. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+        // MARK:- Actions
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK:- Outlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UITextField!
+
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+}
