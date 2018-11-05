1c75293353c74e2a81e53ebd8bac0e54726f5070
Michael Agee <insaineyesay@gmail.com>
2018-11-04 22:06:19 -0500

    moved check mark to left side to allow for detail button to be used to transition to edit screen

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -60,10 +60,11 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
 
     // MARK:- Table View Data Source
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1001) as! UILabel
         if item.checked {
-            cell.accessoryType = .checkmark
+            label.text = "√"
         } else {
-            cell.accessoryType = .none
+            label.text = ""
         }
     }
     
