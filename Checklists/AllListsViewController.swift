cbdcc861886cbb68b103022448489585077dfc95
Michael Agee <insaineyesay@gmail.com>
2018-11-10 22:25:38 -0500

    Adding an all-lists view

---
+++ Checklists/AllListsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Michael Agee on 11/10/18.
+//  Copyright © 2018 Ajira LLC. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+    let cellIdentifier = "ChecklistCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel!.text = "List \(indexPath.row)"
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+    }
+}
